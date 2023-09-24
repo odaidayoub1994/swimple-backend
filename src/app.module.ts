d5609ca43847f6e30f383271673c6a05b4fc72d1
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-09-24 17:26:04 +0400

    🎨 update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UtilsModule } from './common/utils/utils.module';
+import { AppConfigModule } from './configs/app/appConfig.module';
+import { PostgresModule } from './configs/database/postgreSQL/postgreSQLConfig.module';
 
+/**
+ * The root module of application.
+ */
 @Module({
-  imports: [],
+  imports: [AppConfigModule, PostgresModule, UtilsModule],
   controllers: [AppController],
   providers: [AppService],
 })

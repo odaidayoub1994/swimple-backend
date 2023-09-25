4704c01e2ac2c74c58a4b3e2ff04d3e5309d8452
odaidayoub1994 <118967390+odaidayoub1994@users.noreply.github.com>
2023-09-25 13:35:56 +0400

    🚧 updated app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,20 @@ import { AppService } from './app.service';
 import { UtilsModule } from './common/utils/utils.module';
 import { AppConfigModule } from './configs/app/appConfig.module';
 import { PostgresModule } from './configs/database/postgreSQL/postgreSQLConfig.module';
+import { UserModule } from './modules/user/user.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 /**
  * The root module of application.
  */
 @Module({
-  imports: [AppConfigModule, PostgresModule, UtilsModule],
+  imports: [
+    AppConfigModule,
+    PostgresModule,
+    UtilsModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
